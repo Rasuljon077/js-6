140bf297b48f6644e62becf2de2b2ee47e89c32f
rasuljon <anvarovr78@gmail.com>
2024-02-27 19:05:05 +0300

    add index.html

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-let user = prompt("misollarni yechishga harak qilib koring")
+// let user = prompt("misollarni yechishga harak qilib koring")
 let user2 = +prompt("necta misollar ishlamoqchisiz")
 
-for (let i = 0; i < user2; i++) {
+// while (isNaN(user2) || user2 == "0") {
+//     user2 = prompt("iltimos qayta son kirgazing")
+// }
+
+for (let i = 0; i <= user2; i++) {
     
     let a = calculate(1, 5)
     let b = math(1, 1000)
@@ -20,52 +24,80 @@ for (let i = 0; i < user2; i++) {
     function math(min, max) {
         return Math.round(Math.random() * (max - min) + min)
     }
-    
-   var d = +prompt(b + " * " + c + "= ?"  )
-    
+
+ 
     if ( a == 1 ) {
         a = "*"
+         
+      var d = +prompt(b + " * " + c + "= ?"  )
         if (d == e ) {
-            console.log("sizning javob togri " + d);
-        }else{
-            console.log("sizning javob notogri:" + d + " || togri javob " + e);
-        }
-    } else if (a == 2 ){
-        a = "/"
-        if (d == f ) {
             console.log("sizning javob togri " + d);
         }else{
             console.log("sizning javob notogri:" + d + " || togri javob " + f);
         }
+    } else if (a == 2 ){
+        a = "/"
+        if (b < c) {
+           a == 3 || a == 1
+        }else{
+            var k = +prompt(b + "/" + c + "= ?")
+            if (k == f ) {
+            console.log("sizning javob togri " + k);
+            }else{
+            console.log("sizning javob notogri:" + k + " || togri javob " + f);
+           }
+        }
     }else if( a == 3){
           a = "+"
-          if (d == g) {
-            console.log("sizning javob togri " + d);
+          
+    var l = +prompt(b + "+" + c + "= ?")
+          if (l == g) {
+            console.log("sizning javob togri " + l);
           }else{
-            console.log("sizning javob notogri:" + d + " || togri javob " + h);
+            console.log("sizning javob notogri:" + l + " || togri javob " + g);
           }
-    }else if(a ==4 ){
+    }else if(a == 4 ){
         a = "-"
-        if (d == h) {
-            console.log("sizning javob togri " + d);
+        if (b < c ) {
+            a == 3
         }else{
-            console.log("sizning javob notogri:" + d + " || togri javob " + g);
+            var m = +prompt(b + "-" + c + "= ?")
+            if (m == h) {
+                console.log("sizning javob togri " + m);
+            }else{
+                console.log("sizning javob notogri:" + m + " || togri javob " + h);
+            }
         }
+   
     }else{
         a == "%"
-        if (d == j) {
-            console.log("sizning javob togri " + d);
+      if (b < c ) {
+        a == 1
+      }else{
+        var n = +prompt(b + "%" + c + "= ?")
+        if (n == j) {
+            console.log("sizning javob togri " + n);
         }else{
-            console.log("sizning javob notogri:" + d + " || togri javob " + j);
+            console.log("sizning javob notogri:" + n + " || togri javob " + j);
         }
+      }
+  
     } 
 }
+
 
 
 for (let i = 1; i <= 10; i++) {
 
  let name = prompt("ismingizni kiriting")
+ while (!isNaN(name)) {
+    alert("iltimos son kirgazmang")
+    name = prompt("ismingizni qayta kiritishga urining ")
+ }
  let age = prompt("yoshingizni kiriting")
+ while (isNaN(age) || age == "0") {
+    age = +prompt("iltimos yoshingizni qayta kiriting")
+ }
 
    const users = {
       obj:{
